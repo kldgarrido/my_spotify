99d041e16ffeda40ef3938b4e196b0c828ebf2d1
kldgarrido <kldgarrido@gmail.com>
2017-05-20 13:35:54 -0500

    add image_url to album

---
+++ db/migrate/20170520183349_image_url_to_albums.rb
@@ -0,0 +1,5 @@
+class ImageUrlToAlbums < ActiveRecord::Migration[5.0]
+  def change
+  	add_column :albums, :image_url, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170520173926) do
+ActiveRecord::Schema.define(version: 20170520183349) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170520173926) do
     t.integer  "artist_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "image_url"
   end
 
   create_table "artists", force: :cascade do |t|

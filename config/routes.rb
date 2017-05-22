83a48aa151a3092f97b024e4edd136add597882f
Kaled Garrido <kldgarrido@gmail.com>
2017-05-22 09:32:21 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :artists
-  resources :albums
-  resources :songs
+  resources :artists, only: [:index, :show]
 end
